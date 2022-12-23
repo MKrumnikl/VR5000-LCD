5f901b13f5a4e289cf590dd660cda18905569b78
Michal Krumnikl <michal.krumnikl@vsb.cz>
2022-12-23 12:33:01 +0100

    Added step digits

--- vr5000.ino
+++ vr5000.ino
@@ -36,7 +36,8 @@ TFT/OLED (J4)
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke TFT library
 
-#define SERIAL_DEBUG 1
+//#define SERIAL_DEBUG 1
+//#define DEBUG_DIGITS 1
 
 #define HSTEP 16
 #define STATE_INIT 0
@@ -60,27 +61,43 @@ struct sLcdLabels {
   unsigned short bg_color;
 };
 
+struct sLcd7Digits {
+  unsigned short bank_id;
+  unsigned short pos_id;
+  unsigned short bit_id;
+  unsigned short posx;
+  unsigned short posy;
+  unsigned short w;
+  unsigned short h;
+};
+
+#ifdef DEBUG_DIGITS
+unsigned char tmp_bb2[16*8];
+unsigned char tmp_bb4[16*8];
+#endif
+
 // LCD labels cmd[0], cmd[1], cmd[2], cmd[3], text, x, y, foreground, background
 const struct sLcdLabels LcdLabels[] = {
 
   { 4, 0, 9, "F", 3, 2, TFT_BLACK, TFT_WHITE },         //OK
   { 4, 0, 0xA, "BUSY", 33, 2, TFT_BLACK, TFT_GREEN },   //OK
-                                                        // {0, 0, "DSP",   93, 5, 0, TFT_BLACK, TFT_WHITE},
+                                                        // {0, 0, "DSP",   93, 2, 0, TFT_BLACK, TFT_WHITE},
   { 4, 2, 0xD, "ATT", 148, 2, TFT_BLACK, TFT_YELLOW },  //OK
   { 4, 3, 2, "NB", 184, 2, TFT_WHITE, TFT_BLACK },      //OK
-                                                        // {0, 0, "STEP", 230, 5, 0, TFT_BLACK, TFT_WHITE},
-                                                        // {0, 0, "DELAY",381, 5, 0, TFT_BLACK, TFT_WHITE},
-                                                        // {0, 0, "HOLD", 430, 5, 0, TFT_BLACK, TFT_WHITE},
+  { 2, 5, 1, "STEP", 230, 2, TFT_WHITE, TFT_BLACK},     // visible same as Hz
+                                                        // {0, 0, "DELAY",381, 2, 0, TFT_BLACK, TFT_WHITE},
+                                                        // {0, 0, "HOLD", 430, 2, 0, TFT_BLACK, TFT_WHITE},
 
   { 2, 0, 9, "LOCK", 3, 23, TFT_BLACK, TFT_RED },  //OK
-                                                   // {0, 0, "PROG",  40, 28, TFT_BLACK, TFT_WHITE},
-                                                   // {0, 0, "SLEEP", 93, 28, TFT_BLACK, TFT_WHITE},
-                                                   // {0, 0, "ON",   148, 28, TFT_BLACK, TFT_WHITE},
-                                                   // {0, 0, "REC",  184, 28, TFT_BLACK, TFT_WHITE},
+                                                   // {0, 0, "PROG",  40, 23, TFT_BLACK, TFT_WHITE},
+                                                   // {0, 0, "SLEEP", 93, 23, TFT_BLACK, TFT_WHITE},
+                                                   // {0, 0, "ON",   148, 23, TFT_BLACK, TFT_WHITE},
+                                                   // {0, 0, "REC",  184, 23, TFT_BLACK, TFT_WHITE},
   { 2, 2, 7, "AUTO", 230, 23, TFT_WHITE, TFT_BLACK },
-                                                   // {0, 0, "PAUSE",381, 28, TFT_BLACK, TFT_WHITE},
-                                                   // {0, 0, "VCS+", 430, 28, TFT_BLACK, TFT_WHITE},
-
+  { 2, 5, 0,    "K", 346, 23, TFT_WHITE, TFT_BLACK},
+  { 2, 5, 1,   "Hz", 358, 23, TFT_WHITE, TFT_BLACK},
+                                                   // {0, 0, "PAUSE",381, 23, TFT_BLACK, TFT_WHITE},
+                                                   // {0, 0, "VCS+", 430, 23, TFT_BLACK, TFT_WHITE},
   { 2, 0, 4, "LSB", 3, 43, TFT_WHITE, TFT_BLACK },     //OK
   { 2, 0, 0xF, "USB", 45, 43, TFT_WHITE, TFT_BLACK },  //OK
   { 2, 1, 8, "CW", 88, 43, TFT_WHITE, TFT_BLACK },     //OK
@@ -99,6 +116,37 @@ const struct sLcdLabels LcdLabels[] = {
   //  {0, 0, "SEL>",   375, 115, TFT_WHITE, TFT_BLACK },
   //  {0, 0, "PRI>",   374, 131, TFT_WHITE, TFT_BLACK }
 
+};
+
+// 7segment digits 
+const struct sLcd7Digits Lcd7Digits[] = {
+  {4, 4, 0xA, 282   , 2+3 , 3, 13 },  // f 1st step
+  {4, 4, 9  , 282+3 , 2+16, 11, 3 },  // g 1st step
+  {4, 4, 8  , 282+14, 2+3 , 3, 13 },  // b 1st step
+  {4, 5, 7  , 282+3 , 2   , 11, 3 },  // a 1st step
+  {2, 4, 9  , 282+3 , 2+32, 11, 3 },  // d 1st step
+  {2, 4, 8  , 282+14, 2+19, 3 ,13 },  // c 1st step
+  {2, 4, 0XA, 282   , 2+19, 3 ,13 },  // e 1st step
+
+  {2, 4, 7  , 282+18, 2+34, 3 , 3 },  // dot 1st step
+
+  {4, 4, 6  , 304   , 2+3 , 3, 13 },  // f 2nd step
+  {4, 4, 5  , 304+3 , 2+16, 11, 3 },  // g 2nd step
+  {4, 4, 4  , 304+14, 2+3 , 3, 13 },  // b 2nd step
+  {4, 5, 0xE, 304+3 , 2   , 11, 3 },  // a 2nd step
+  {2, 4, 5  , 304+3 , 2+32, 11, 3 },  // d 2nd step
+  {2, 4, 4  , 304+14, 2+19, 3 ,13 },  // c 2nd step
+  {2, 4, 6  , 304   , 2+19, 3 ,13 },  // e 2nd step
+
+  {2, 4, 3  , 304+18, 2+34, 3 , 3 },  // dot 2nd step
+
+  {4, 5, 0xF, 326   , 2+3 , 3, 13 },  // f 3rd step
+  {4, 4, 1  , 326+3 , 2+16, 11, 3 },  // g 3rd step
+  {4, 4, 0  , 326+14, 2+3 , 3, 13 },  // b 3rd step
+  {4, 5, 0xF, 326+3 , 2   , 11, 3 },  // a 3rd step
+  {4, 4, 2  , 326+3 , 2+32, 11, 3 },  // d 3rd step
+  {2, 4, 0  , 326+14, 2+19, 3 ,13 },  // c 3rd step
+  {2, 4, 2  , 326   , 2+19, 3 ,13 }   // e 3rd step
 };
 
 // edit  packages/STMicroelectronics/hardware/stm32/2.3.0/libraries/SrcWrapper/src/stm32/interrupt.cpp
@@ -156,7 +204,7 @@ void setup(void) {
   attachInterrupt(digitalPinToInterrupt(PA2), NULL, FALLING);
 
 #ifdef SERIAL_DEBUG
-  Serial.begin(115200);
+  Serial.begin(921600);
   Serial.println("Waiting ...");
   Serial.print("CPU SPEED: ");
   Serial.println(F_CPU);
@@ -207,8 +255,10 @@ void parseNext(unsigned short c) {
   }
   if (state == STATE_DATA) {
     
+    
     // start with display symbols
     if ((cmd[0] == 0x0BB4 || cmd[0] == 0x0BB2) && cmd_idx == 3) {
+      // labels
       for (int i = 0; i < sizeof(LcdLabels); i++) {
         if (((cmd[0] & 0x000F) == LcdLabels[i].bank_id) && ((cmd[1] & 0x000F) == LcdLabels[i].pos_id) && ((cmd[2] & 0x000F) == LcdLabels[i].bit_id)) {
           tft.setCursor(LcdLabels[i].posx, LcdLabels[i].posy);
@@ -220,7 +270,42 @@ void parseNext(unsigned short c) {
           tft.print(LcdLabels[i].text);
         }
       }
+      // 7digits
+      for (int i = 0; i < sizeof(Lcd7Digits); i++) {
+        if (((cmd[0] & 0x000F) == Lcd7Digits[i].bank_id) && ((cmd[1] & 0x000F) == Lcd7Digits[i].pos_id) && ((cmd[2] & 0x000F) == 0xF - Lcd7Digits[i].bit_id)) {
+          if ((cmd[4] & 0x0001) != 0) {
+            mFillRect(Lcd7Digits[i].posx, Lcd7Digits[i].posy, Lcd7Digits[i].w, Lcd7Digits[i].h, TFT_WHITE);
+          } else {
+            mFillRect(Lcd7Digits[i].posx, Lcd7Digits[i].posy, Lcd7Digits[i].w, Lcd7Digits[i].h, TFT_BLACK);
+          }
+        }
+      }
     }
+    
+#ifdef DEBUG_DIGITS    
+    if ((cmd[0] == 0x0BB4 || cmd[0] == 0x0BB2) && cmd_idx == 3) {
+        if (cmd[0] == 0x0BB2) {
+          tmp_bb2[((cmd[1] & 0x000F)*16 + (cmd[2] & 0x000F))] = cmd[4];
+          Serial.print("BB2 ");
+          for (int i = 16*4; i < 16*8; i++) {
+            if (i % 16 == 0) Serial.print(" | ");
+            Serial.print(tmp_bb2[i], HEX);
+            Serial.print(" ");
+          }
+          Serial.println();
+        }
+        if (cmd[0] == 0x0BB4) {
+          tmp_bb4[((cmd[1] & 0x000F)*16 + (cmd[2] & 0x000F))] = cmd[4];
+           Serial.print("BB4 ");
+           for (int i = 16*4; i < 16*8; i++) {
+            if (i % 16 == 0) Serial.print(" | ");
+            Serial.print(tmp_bb4[i], HEX);
+            Serial.print(" ");
+          }
+          Serial.println();
+        }  
+    }
+#endif
 
     if (c == 0x0BEE && cmd_idx > 1) {
       state = STATE_INIT;
